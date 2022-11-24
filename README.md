68135fde594a11d6275e3d124f69f056a352fa55
Vernon Grant <vernon@ruppell.io>
2022-11-24 13:05:06 +0700

    Added Windows terminal WSL helper function.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # win-helpers.el
+
 A series of helper functions for Windows Emacs users that are making use of WSL.
+**Still in development**
+
+## Provided functions
+
+| Function Name                  | Description                                                                                                           |
+|--------------------------------|-----------------------------------------------------------------------------------------------------------------------|
+| `wh-wsl-open-windows-terminal` | Opens the windows terminal from within WSL in the current directory. Takes an optional argument for the profile name. |
+|                                |                                                                                                                       |
+
+## Binding examples
+
+```lisp
+
+;; Open Windows terminal from within WSL in the current folder.
+(define-key global-map (kbd "C-c t") 'wh-wsl-open-windows-terminal)
+
+;; Open Windows terminal, with the powershell profile, from within WSL in the current folder.
+(define-key global-map (kbd "C-c t")
+  (lambda ()
+    (interactive)
+    (wh-wsl-open-windows-terminal "PowerShell")))
+```
---
+++ win-helpers.el
@@ -0,0 +1,72 @@
+;;; win-helpers.el --- Provides a series of Emacs helper functions for WSL Windows users.  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 by Vernon Grant.
+
+;; Author: Vernon Grant <vernon@ruppell.io>
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: Windows, helpers, package
+;; Homepage: https://github.com/VernonGrant/win-helpers.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Provides a series of Emacs helper functions for WSL Windows users.
+
+;;; Code:
+
+;;;;;;;;;;;;;;;;;;;;
+;; Customizations ;;
+;;;;;;;;;;;;;;;;;;;;
+
+(defgroup win-helpers nil
+    "Windows helpers settings."
+    :group 'convenience
+    :prefix "win-helpers-")
+
+;;;;;;;;;;;;;;;
+;; Utilities ;;
+;;;;;;;;;;;;;;;
+
+(defun wh-shell-command-no-window (command)
+  "Run the provided command in the background.
+COMMAND: The shell command."
+  (interactive)
+  (let
+      ((display-buffer-alist
+        (list
+         (cons
+          "\\*Async Shell Command\\*.*"
+          (cons #'display-buffer-no-window nil)))))
+    (async-shell-command
+     command)))
+
+;;;;;;;;;;;;;;;;;
+;; WSL Helpers ;;
+;;;;;;;;;;;;;;;;;
+
+;; Add Wayland clipboard fixes.
+
+(defun wh-wsl-open-windows-terminal(&optional profile)
+    "Opens the Windows terminal from within WSL in the current directory.
+PROFILE: The terminal profile name (Optional)."
+    (interactive)
+    (if (not profile)
+        (wh-shell-command-no-window "wt.exe -d .")
+        (wh-shell-command-no-window (concat "wt.exe -p \"" profile "\" -d ."))))
+
+(provide 'win-helpers)
+
+;;; win-helpers.el ends here
